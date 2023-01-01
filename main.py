59627291efc3e3ff9ff6a75a90f3b309fe0e3aa6
Timo <timo.fischer@protonmail.com>
2023-01-01 20:09:23 +0100

    Create class for products

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ class Employee:
         """
         self.name = name
         self.salary = salary
+
+class Product:
+    def __init__(self, name, price, stock):
+        """
+        Initialize a product with a name, price and stock level.
+        
+        Parameters:
+        name (str): The name of the product.
+        price (int): The price of the product.
+        stock (int): The stock level of the product.
+        """
+        self.name = name
+        self.price = price
+        self.stock = stock
