42d30a2328600036f68252bb506a865a1616ada0
Timo <timo.fischer@protonmail.com>
2023-01-01 19:45:56 +0100

    Add class for employees

---
+++ main.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self, name, salary):
+        """
+        Initialize an Employee with a name and salary.
+        
+        Parameters:
+        - name (str): The name of the employee.
+        - salary (int): The salary of the employee.
+        """
+        self.name = name
+        self.salary = salary
